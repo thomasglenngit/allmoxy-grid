202368a726e8ce67a8a33d7eebbce816b8b20ba9
Thomas Glenn <thomasgrantglenn@mac.com>
2021-05-03 12:34:53 -0600

    add App, Form, Header, README and grid image.

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header'
+import Form from './Form'
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <div className="ui container">
+        <Header />
+        <Form />
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default App
---
+++ src/components/Form.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <React.Fragment>
+      <div className="ui five column relaxed grid">
+        <div className="column">
+          Title
+  </div>
+        <div className="column">
+          Description
+  </div>
+        <div className="column">
+          Price
+  </div>
+        <div className="column">
+          Quantity
+  </div>
+        <div className="column">
+          Image
+  </div>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default Form
